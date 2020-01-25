a1794aa484d736441c6b5414605ec8f23cd1af71
Avanti Mulay <avanti@localhost.localdomain>
2020-01-26 00:01:56 +0530

    Updates

---
+++ precache-manifest.35dfdc9872791f7c8b246d216bf95f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617fce7e9179655b63a8fc15655d7072",
+    "url": "/N-Puzzle/index.html"
+  },
+  {
+    "revision": "d2863204e01716bbff24",
+    "url": "/N-Puzzle/static/css/main.2d8f5f26.chunk.css"
+  },
+  {
+    "revision": "6ebf61eefe7a472278c8",
+    "url": "/N-Puzzle/static/js/2.d8e5eaf9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/N-Puzzle/static/js/2.d8e5eaf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2863204e01716bbff24",
+    "url": "/N-Puzzle/static/js/main.1fe5a437.chunk.js"
+  },
+  {
+    "revision": "5ad83e01306808e1d784",
+    "url": "/N-Puzzle/static/js/runtime-main.4cceb170.js"
+  }
+]);
